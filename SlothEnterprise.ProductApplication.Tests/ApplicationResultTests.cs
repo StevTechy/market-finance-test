74a4131174ca31a1b8f51c92e8b3152c9601fd65
Steven <stevenms@outlook.com>
2020-11-22 17:06:25 +0000

    Set up extension method to convert IApplicationResult

---
+++ SlothEnterprise.ProductApplication.Tests/ApplicationResultTests.cs
@@ -0,0 +1,40 @@
+﻿using Moq;
+using SlothEnterprise.External;
+using SlothEnterprise.ProductApplication.Applications;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace SlothEnterprise.ProductApplication.Tests
+{
+    /// <summary>
+    /// These tests ensure the correct enum result is being returned after an Application has been submitted
+    /// </summary>
+    public class ApplicationResultTests
+    {
+        private IApplicationResult GetSuccessfulApplicationResult()
+        {
+            return Mock.Of<IApplicationResult>(a => a.ApplicationId == 1 && a.Success == true);
+        }
+
+        private IApplicationResult GetFailedApplicationResult()
+        {
+            return Mock.Of<IApplicationResult>(a => a.ApplicationId == 1 && a.Success == false);
+        }
+
+        private IApplicationResult GetApplicationResultWithNoApplicationId()
+        {
+            return Mock.Of<IApplicationResult>();
+        }
+
+        [Fact]
+        public void GetReturnCodeFromApplicationResult()
+        {
+            var result = GetSuccessfulApplicationResult();
+            Assert.Equal(ApplicationResultEnum.Success, GetSuccessfulApplicationResult().GetReturnCodeFromApplicationResult());
+            Assert.Equal(ApplicationResultEnum.Failed, GetFailedApplicationResult().GetReturnCodeFromApplicationResult());
+            Assert.Equal(ApplicationResultEnum.Failed, GetApplicationResultWithNoApplicationId().GetReturnCodeFromApplicationResult());
+        }
+    }
+}
--- SlothEnterprise.ProductApplication/Applications/SellerApplication.cs
+++ SlothEnterprise.ProductApplication/Applications/SellerApplication.cs
@@ -1,4 +1,5 @@
-﻿using SlothEnterprise.ProductApplication.Products;
+﻿using SlothEnterprise.External;
+using SlothEnterprise.ProductApplication.Products;
 
 namespace SlothEnterprise.ProductApplication.Applications
 {
@@ -13,5 +14,26 @@ namespace SlothEnterprise.ProductApplication.Applications
     {
         public IProduct Product { get; set; }
         public ISellerCompanyData CompanyData { get; set; }
+    }
+
+    public static class ApplicationResultExtensions
+    {
+        /// <summary>
+        /// Converts an ApplicationResult into an enumeration value based on whether the application was successfully created
+        /// </summary>
+        /// <param name="result">The application result returned from the service handling an application submission</param>
+        /// <returns>Enumeration value, Failed: 0, Success: 1</returns>
+        public static ApplicationResultEnum GetReturnCodeFromApplicationResult(this IApplicationResult result)
+        {
+            if (result.Success && result.ApplicationId != null)
+                return ApplicationResultEnum.Success;
+            else return ApplicationResultEnum.Failed;
+        }
+    }
+
+    public enum ApplicationResultEnum
+    {
+        Failed = 0,
+        Success = 1
     }
 }
