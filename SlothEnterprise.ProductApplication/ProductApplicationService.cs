6ab85a09528942a918c5c5c87784c69680a52348
Steven <stevenms@outlook.com>
2020-11-22 18:31:20 +0000

    Update SubmitApplicationFor to use Submit from ISellerApplication

--- SlothEnterprise.ProductApplication/ProductApplicationService.cs
+++ SlothEnterprise.ProductApplication/ProductApplicationService.cs
@@ -8,56 +8,9 @@ namespace SlothEnterprise.ProductApplication
 {
     public class ProductApplicationService
     {
-        private readonly ISelectInvoiceService _selectInvoiceService;
-        private readonly IConfidentialInvoiceService _confidentialInvoiceWebService;
-        private readonly IBusinessLoansService _businessLoansService;
-
-        public ProductApplicationService(ISelectInvoiceService selectInvoiceService, IConfidentialInvoiceService confidentialInvoiceWebService, IBusinessLoansService businessLoansService)
-        {
-            _selectInvoiceService = selectInvoiceService;
-            _confidentialInvoiceWebService = confidentialInvoiceWebService;
-            _businessLoansService = businessLoansService;
-        }
-
         public int SubmitApplicationFor<T>(ISellerApplication<T> application) where T : IProduct
         {
-
-            if (application.Product is SelectiveInvoiceDiscount sid)
-            {
-                return _selectInvoiceService.SubmitApplicationFor(application.CompanyData.Number.ToString(), sid.InvoiceAmount, sid.AdvancePercentage);
-            }
-
-            if (application.Product is ConfidentialInvoiceDiscount cid)
-            {
-                var result = _confidentialInvoiceWebService.SubmitApplicationFor(
-                    new CompanyDataRequest
-                    {
-                        CompanyFounded = application.CompanyData.Founded,
-                        CompanyNumber = application.CompanyData.Number,
-                        CompanyName = application.CompanyData.Name,
-                        DirectorName = application.CompanyData.DirectorName
-                    }, cid.TotalLedgerNetworth, cid.AdvancePercentage, cid.VatRate);
-
-                return (result.Success) ? result.ApplicationId ?? -1 : -1;
-            }
-
-            if (application.Product is BusinessLoans loans)
-            {
-                var result = _businessLoansService.SubmitApplicationFor(new CompanyDataRequest
-                {
-                    CompanyFounded = application.CompanyData.Founded,
-                    CompanyNumber = application.CompanyData.Number,
-                    CompanyName = application.CompanyData.Name,
-                    DirectorName = application.CompanyData.DirectorName
-                }, new LoansRequest
-                {
-                    InterestRatePerAnnum = loans.InterestRatePerAnnum,
-                    LoanAmount = loans.LoanAmount
-                });
-                return (result.Success) ? result.ApplicationId ?? -1 : -1;
-            }
-
-            throw new InvalidOperationException();
+            return application.Submit() == ApplicationResultEnum.Success ? 1 : 0;
         }
     }
 }
